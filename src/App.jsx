6b982bb3d4875dee46800ac5f643cdf4a7f31c06
Lakshya <malhotralakshya88@gmail.com>
2025-12-22 20:46:26 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const App = () => {
+  return (
+    <div>
+    <h1 className='text-amber-500 font-serif underline capitalize text-9xl'> Welcome to the sky cast </h1>
+    </div>
+  )
+}
+
+export default App
