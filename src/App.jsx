2b41e24374e54298e2c4de7afc40d5cac3d002d8
Lakshya <malhotralakshya88@gmail.com>
2025-12-23 10:42:46 +0530

    Png's Added

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,7 @@ const App = () => {
   return (
     <div>
     <h1 className='text-amber-500 font-serif underline capitalize text-9xl'> Welcome to the sky cast </h1>
+    <h2 className=' text-3xl text-red-500 font-serif text-center'>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ipsa maiores eius necessitatibus sequi, eligendi illum nobis ratione aut commodi culpa.</h2>
     </div>
   )
 }
